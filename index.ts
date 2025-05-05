8a8e063a546e3741fe15b7217d4e057305059b83
MRamadanMcit <mramadan.mcit@gmail.com>
2025-05-05 12:47:51 +0300

    1- Introduction

---
+++ index.ts
@@ -0,0 +1,12 @@
+// What is TypeScript?
+// It's open source programming language and developed by Microsoft
+// Build on JS and added more features 
+// We can use it in front end an back end
+
+// _______________________________________________________________
+
+// What should I learn TypeScript?
+// Reduce errors 
+// Best choice for large development
+// Big companies use TS
+
