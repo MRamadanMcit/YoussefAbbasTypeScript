a4114e5100ac808726a616e8030271dc1c022d60
MRamadanMcit <mramadan.mcit@gmail.com>
2025-05-06 13:33:34 +0300

    3 - tsconfig.json file

---
+++ dist/index.js
@@ -0,0 +1,3 @@
+"use strict";
+let price = 10;
+let isValid;
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+// To install tsconfig
+// tsc --init
+
+//______________________________________________________
+
+// In tsconfig file
+// To active rootDir to "rootDir": "./src", and put index.ts in folder src
+// To active outDir to "outDir": "./dist", to out in dist
+// To active noEmitOnError to "noEmitOnError": true, to if has error not allow to compile
+// To active removeComments to "removeComments": true, to Remove all comments
+
+//______________________________________________________
+
+// To compile to Js
+// tsc
+
+//______________________________________________________
+
+let price: number = 10;
+let isValid: boolean;
