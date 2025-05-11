221d9378a0079b0e519325adac33a356432058a3
MRamadanMcit <mramadan.mcit@gmail.com>
2025-05-11 12:36:59 +0300

    7 - Enum

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,12 @@
 "use strict";
-const employee = [2, "mohamed"];
-const user = [2, "mohamed", true];
+const small = 0;
+const medium = 1;
+const large = 2;
+var size;
+(function (size) {
+    size[size["small"] = 0] = "small";
+    size[size["medium"] = 1] = "medium";
+    size[size["large"] = 2] = "large";
+})(size || (size = {}));
+const mySize = size.medium;
+console.log({ mySize });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,33 @@
+// node dist/index.js                    // To print console in terminal
+
 //______________________________________________________
 
-// Tuple in TypeScript
-const employee: [number, string] = [2, "mohamed"];
-const user: [number, string, boolean] = [2, "mohamed", true]; // Tuple is array with fixed length 
+// Enum in TypeScript
+
+// Js Way
+const small = 0;
+const medium = 1;
+const large = 2;
+
+// TS Way
+// enum size {
+//   small,
+//   medium,
+//   large,
+// }
+
+// enum size {
+//   small,
+//   medium = 5,
+//   large,
+// }
+// we can assign number in enum and next variable takes a sequence number by default and once if add string to one variable we should add to others
+
+enum size {
+  small = "small",
+  medium = "medium",
+  large = "large",
+}
+
+const mySize: size = size.medium;
+console.log({ mySize });
